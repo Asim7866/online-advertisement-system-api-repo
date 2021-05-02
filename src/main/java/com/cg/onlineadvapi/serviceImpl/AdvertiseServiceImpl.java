0d0148a101a58e7c102b474e0819f691394e8945
strehan <strehan@LIN25003848.corp.capgemini.com>
2021-05-02 13:57:24 +0530

    Updated Services Impl with Logger - shivam

--- src/main/java/com/cg/onlineadvapi/serviceImpl/AdvertiseServiceImpl.java
+++ src/main/java/com/cg/onlineadvapi/serviceImpl/AdvertiseServiceImpl.java
@@ -2,6 +2,8 @@ package com.cg.onlineadvapi.serviceImpl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,16 +13,20 @@ import com.cg.onlineadvapi.repository.AdvertiseRepository;
 import com.cg.onlineadvapi.service.AdvertiseService;
 @Service
 public class AdvertiseServiceImpl implements AdvertiseService {
+	Logger logger = LoggerFactory.getLogger(AdvertiseServiceImpl.class);
 	@Autowired
 	private AdvertiseRepository advertiseRepository;
 	@Override
 	public ResponseEntity<Object> viewAdvertisementByUser(int userId) {
 		// Fetching Advertisement uploaded by specific user using UserId
-		
+		logger.info("Fetching Advertisement of User with UserID="+userId);
 		List<Advertise> fetchedData=advertiseRepository.viewAdvertisementByUser(userId);
 		if(fetchedData.isEmpty())   //if an empty list is returned, error message is returned
+			{logger.error("No Advertisement found of User with UserID="+userId);
 			return ResponseEntity.accepted().body("No advertisement found for inputed user ID"); 
+			}
 		//else, fetchedData is returned
+		logger.info("Found Advertisement of User with UserID="+userId);
 		return ResponseEntity.accepted().body(fetchedData); 
 
 	}
@@ -28,10 +34,14 @@ public class AdvertiseServiceImpl implements AdvertiseService {
 	@Override
 	public String deleteAdvertise(int advertisementId) {
 		// delete Advertisement using AdvertisementId
+		logger.info("Deleting Advertisement with AdvertisementID="+advertisementId);
 		try{advertiseRepository.deleteById(advertisementId);}
+		//if advertisement not found :
 		catch(Exception e) {
+			logger.error("Advertisement not found with AdvertisementID="+advertisementId);
 			return("AdvertisementId not found");} 
-		
+		//if advertisement found :
+		logger.info("Advertisement Deleted with AdvertisementID="+advertisementId);
 		return "Advertisement deleted Successfully";
 	}
 
--- src/main/java/com/cg/onlineadvapi/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/cg/onlineadvapi/serviceImpl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cg.onlineadvapi.serviceImpl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,14 +9,18 @@ import com.cg.onlineadvapi.repository.UserRepository;
 import com.cg.onlineadvapi.service.UserService;
 @Service
 public class UserServiceImpl implements UserService {
+	Logger logger = LoggerFactory.getLogger(AdvertiseServiceImpl.class);
 	@Autowired
 	private UserRepository userRepository;
 	@Override
 	public String deleteUser(int userId) {
 		// delete User using UserId
+		logger.info("Deleting User with UserID="+userId);
 		try{userRepository.deleteById(userId);}
 		catch(Exception e) {
+			logger.error("No user found with UserID="+userId);
 			return("UserId not found");} //if user id not found, error message is returned 
+		logger.info("User deleted with UserID="+userId);
 		return "User Deleted Successfully";	//if user id found, successful execution message is returned
 	}
 
