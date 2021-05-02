d66c0ade91612a6864961d085eb69dea73f2b2e1
Asim7866 <83334582+Asim7866@users.noreply.github.com>
2021-05-02 08:55:37 +0530

    Update AddressRepository.java

--- src/main/java/com/cg/onlineadvapi/repository/AddressRepository.java
+++ src/main/java/com/cg/onlineadvapi/repository/AddressRepository.java
@@ -1,5 +1,8 @@
 package com.cg.onlineadvapi.repository;
-
-public class AddressRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.cg.onlineadvapi.domain.Address;
+@Repository
+public class AddressRepository extends JpaRepository<Address, Integer>{
 
 }
