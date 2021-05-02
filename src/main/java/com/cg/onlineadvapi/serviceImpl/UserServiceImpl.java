662c96e782edda7930cebb964355dbfdd8ddfca6
Asim7866 <83334582+Asim7866@users.noreply.github.com>
2021-05-02 09:01:54 +0530

    Update UserServiceImpl.java

--- src/main/java/com/cg/onlineadvapi/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/cg/onlineadvapi/serviceImpl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cg.onlineadvapi.serviceImpl;
-
-public class UserServiceImpl {
+import org.springframework.stereotype.Service;
+@Service
+public class UserServiceImpl implements UserService{
 
 }
